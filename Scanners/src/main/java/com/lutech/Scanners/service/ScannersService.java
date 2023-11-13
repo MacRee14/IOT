b001efa9a8138630fc1b23ec362ace823cc038c8
AleDeri <alessaderi@gmail.com>
2023-11-13 11:14:37 +0100

    mysql pom update

--- Scanners/src/main/java/com/lutech/Scanners/service/ScannersService.java
+++ Scanners/src/main/java/com/lutech/Scanners/service/ScannersService.java
@@ -15,10 +15,10 @@ public class ScannersService {
     private List<Scanners> scans = new ArrayList<Scanners>();
     @Autowired
     public ScannersService(){
-        scans.add(new Scanners(1, 1, LocalDate.of(2020, Month.JANUARY, 18).atTime(3,00), 500));
-        scans.add(new Scanners(2, 1, LocalDate.of(2020, Month.JANUARY, 18).atTime(3,10), 450));
-        scans.add(new Scanners(3, 2, LocalDate.of(2020, Month.JANUARY, 18).atTime(4,00), 600));
-        scans.add(new Scanners(3, 2, LocalDate.of(2020, Month.JANUARY, 18).atTime(4,00), 600));
+        scans.add(new Scanners(1, 1, LocalDate.of(2023, Month.NOVEMBER, 10).atTime(3,00), 500));
+        scans.add(new Scanners(2, 1, LocalDate.of(2023, Month.NOVEMBER, 10).atTime(3,10), 450));
+        scans.add(new Scanners(3, 2, LocalDate.of(2023, Month.NOVEMBER, 11).atTime(4,00), 600));
+        scans.add(new Scanners(4, 2, LocalDate.of(2023, Month.NOVEMBER, 11).atTime(4,10), 700));
     }
     public List<Scanners> getAllScans(){
         return scans;
